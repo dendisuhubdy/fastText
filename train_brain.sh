12b7017be1a2f5a154605c6cdf8906ea294ade6c
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2019-04-08 07:19:08 +0000

    working multiple threads with customer interest matrix

--- train_brain.sh
+++ train_brain.sh
@@ -5,13 +5,12 @@ EXPNAME=`date "+%Y-%m-%d_%H-%M-%S_%N"`
 mkdir -p exp/$EXPNAME
 echo "Starting $EXPNAME"...
 
-gdb --args fasttext skipgram \
-    -input data/random_hist.txt \
+fasttext skipgram \
+    -input /home/ubuntu/data/customer_interest_matrix.txt \
     -output exp/$EXPNAME/model \
     -lr 0.05 \
     -dim 300 \
     -minCount 1 \
     -loss ns \
-    -thread 1 \
-    -epoch 2000 \
-    -printEvery 10
+    -thread 8 \
+    -epoch 2000 2>&1 | tee -i exp/$EXPNAME/log
